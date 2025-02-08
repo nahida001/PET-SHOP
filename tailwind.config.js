7a2f395ddcc84c63166aa9e9b492b370aa8d79d7
NAHIDA SULTANA <nahidas705@gmail.com>
2025-02-08 22:01:39 +0600

    added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/img/hero-pattern.svg')",
+          'hero-pattern': "url('/c1')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
